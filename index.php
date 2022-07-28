037f246797e1aac431238d75291790cf5ecba816
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-07-28 21:07:16 +0900

    searchshops-hanayama

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/reply.php';
+require_once __DIR__ . '/search.php';
+// テーブル名を定義
+//ユーザデータテーブル名(直前に送信したデータを取り込んでおく)
+define('TABLE_NAME_USERS', 'users');
+//ユーザの感想テーブル名
+define('TABLE_NAME_REVIEWS', 'reviews');
+//店の情報テーブル名(テストで3件のみ)
+define('TABLE_NAME_SHOPS', 'shops');
+/*テーブルデータ(★:PRIMARY, ☆:FOREIGN)
+users(
+    ★userid(bytea)...ユーザID
+    before_send(text)...直前のメッセージ
+)
+reviews(
+    ★review_no(integer)...レビューを一意にするための番号
+    ☆shopid(text)...登録された店舗のID
+    ☆userid(bytea)...登録したユーザID
+    evaluation(interger)...全体の評価
+    free(text)...自由欄
+)
+shops(テスト用、実際はマップ等から選んでレビューを書けるようにする予定)(
+    ★shopid(text)...店舗のID
+    shopname(text)...店舗名
+)
+*/
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
@@ -28,6 +53,17 @@ foreach ($events as $event) {
     if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
         error_log('Non message event has come');
         continue;
+    }
+
+    //メッセージに対する返答
+    if(strcmp($event->getText(), "お店を探す") == 0) {
+        #データベースから位置情報取得
+        #テスト用の位置情報
+        $lat = 36.063513;
+        $lon = 136.222748;
+        $return_message_text = get_restaurant_information($lat, $lon);
+        $response_format = text_format($return_message_text);
+        replyTextMessage($bot, $event->getReplyToken(), $response_format);
     }
 
     //メッセージに対する返答
