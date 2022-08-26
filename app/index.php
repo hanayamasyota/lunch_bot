050aafec51baf540c893880c73e1c2b87d587581
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-08-26 16:58:58 +0900

    update-hanayama

---
+++ app/index.php
@@ -0,0 +1,392 @@
+<?php
+//web: vendor/bin/heroku-php-nginx -C nginx_app.conf
+//web: vendor/bin/heroku-php-nginx app/
+
+// load files
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/reply.php';
+require_once __DIR__ . '/search.php';
+require_once __DIR__ . '/database_function/database_function.php';
+
+// テーブル名を定義
+//ユーザデータテーブル名(直前に送信したデータを取り込んでおく)
+define('TABLE_NAME_USERS', 'users');
+//ユーザの検索結果のデータを保持する
+define('TABLE_NAME_USERSHOPDATA', 'usershopdata');
+//ユーザの感想テーブル名
+define('TABLE_NAME_REVIEWS', 'reviews');
+//レビューの内容を一時的にストックするテーブル名
+define('TABLE_NAME_REVIEWSTOCK', 'reviewstock');
+//店の情報テーブル名(テストで3件のみ)
+define('TABLE_NAME_SHOPS', 'shops');
+//個人の検索結果データ
+define('TABLE_NAME_NAVIGATION', 'navigation');//未実装
+
+//1ページ当たりの表示件数(後から変更できるように)
+define('PAGE_COUNT', 5);
+/*テーブルデータ(★:PRIMARY KEY, ☆:FOREIGN)
+users(
+    ★userid(bytea)...ユーザID
+    before_send(text)...直前のメッセージ
+    latitude(float)...緯度
+    longitude(float)...経度
+    追加
+    favolite_genre...お気に入りのジャンル
+    search_range...検索範囲
+    rest_start...休憩の始まる時間
+    rest_end...休憩の終わる時間
+)
+usershopdata(
+    ★userid(bytea)
+    page_num(integer)...検索結果の現在のページ数
+    review_shop(text)...レビュー中の店舗ID
+    shop_length(integer)...検索件数
+)
+reviews(あとから変更や削除ができるようにする。自分が書いたレビューを見れるようにする。
+    ★review_no(serial)...レビューを一意にするための番号
+    shopid(text)...登録された店舗のID
+    userid(bytea)...登録したユーザID
+    evaluation(integer)...全体の評価
+    recommend(text)...おすすめメニュー
+    free(text)...自由欄
+)
+reviewstock(レビューのデータをストックしておくテーブル、キャンセル時・コミット時には消去する)(
+    userid(bytea)...ユーザID
+    shopid(text)...店舗ID
+    review_1(integer)...全体の評価
+    review_2(text)...おすすめメニュー
+    review_3(text)...自由欄
+)
+reviews(新)(
+    userid(bytea)
+    shopid(text)
+    review_num(int)...レビューの順番
+    review(text)
+)
+shops(
+    ★shopid(text)...店舗のID
+    shopname(text)...店舗名
+)
+navigation(お店を探すとレビューで使用)(
+    ★userid(bytea)...ユーザIDと店舗IDの複合主キー
+    ★shopid(text)...
+    shopnum(int)...店の表示順に番号を付ける
+    shopname(text)...店名
+    shop_lat(float)...店の緯度(apiから取得)
+    shop_lng(float)...店の経度    
+)
+
+*/
+
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+// LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+// signature check
+try {
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+    error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+    error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+    error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+    error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+}
+
+//main//----------------------------------------------------------------
+foreach ($events as $event) {
+
+    // 位置情報メッセージ
+    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\LocationMessage) {
+        if (getBeforeMessageByUserId($event->getUserId()) === 'location_set') {
+            // usersテーブルに緯度経度を設定
+            $lat = $event->getLatitude();
+            $lon = $event->getLongitude();
+            updateLocation($event->getUserId(), $lat, $lon);
+            updateUser($event->getUserId(), null);
+            replyTextMessage($bot, $event->getReplyToken(),
+            '位置情報を設定しました。');
+        }
+    }
+
+    // postbackイベント
+    if ($event instanceof \LINE\LINEBot\Event\PostbackEvent) {
+        if (getBeforeMessageByUserId($event->getUserId()) === 'shop_search') {
+            // review_write_...
+            if (strpos($event->getPostbackData(), 'review_write_') !== false) {
+                // postbackテキストからidを抜き出す
+                $shopNum = intval(explode('_', $event->getPostbackData())[2]);
+                $id = explode('_', $event->getPostbackData())[3];
+                updateUserShopData($event->getUserId(), 'review_shop', $id);
+                replyTextMessage($bot, $event->getReplyToken(), $shopNum);
+            }
+        }
+        continue;
+    }
+
+    // キャンセル
+    if (strcmp($event->getText(), 'キャンセル') == 0) {
+        // before_sendの有無を確認、ない場合はスルー
+        if ((getBeforeMessageByUserId($event->getUserId()) != PDO::PARAM_NULL) && (getBeforeMessageByUserId($event->getUserId()) != null)) {
+            $mode = '';
+            // shop_reviewを含む場合
+            if (strpos(getBeforeMessageByUserId($event->getUserId()), 'shop_review') !== false) {
+                if (getUserIdCheck($event->getUserId(), TABLE_NAME_REVIEWSTOCK) != PDO::PARAM_NULL) {
+                    //reset reviewstock
+                    deleteUser($event->getUserId(), TABLE_NAME_REVIEWSTOCK);
+                }
+                $mode = 'お店のレビュー';
+            }
+            // location_setを含む場合
+            else if (strpos(getBeforeMessageByUserId($event->getUserId()), 'location_set') !== false) {
+                $mode = '位置情報の設定';
+            }
+            // shop_searchを含む場合
+            else if (strpos(getBeforeMessageByUserId($event->getUserId()), 'shop_search') !== false) {
+                $mode = 'お店を探す';
+            }
+            // 共通部分
+            updateUser($event->getUserId(), null);
+            replyTextMessage($bot, $event->getReplyToken(),
+            '「'.$mode.'」がキャンセルされました。');
+        }
+
+    // レビューを書くかの場面で「はい」と送信された場合
+    } else if ((getBeforeMessageByUserId($event->getUserId()) === 'shop_review_0') && (strcmp($event->getText(), 'はい') == 0)) {
+        updateUser($event->getUserId(), 'shop_review_1');
+    // 総合の評価を入力する場面で1~5の数字が送信された場合
+    } else if ((getBeforeMessageByUserId($event->getUserId()) === 'shop_review_1') && (preg_match('/^[1-5]{1}/', $event->getText()))) {
+        // insert reviewstock
+        $evaluation = intval($event->getText());
+        updateReviewData($event->getUserId(), 'review_1', $evaluation);
+        // update before_send
+        updateUser($event->getUserId(), 'shop_review_2');
+    // おすすめメニューを仮のテーブル(reviewstock)に登録
+    } else if ((getBeforeMessageByUserId($event->getUserId()) === 'shop_review_2')) {
+        updateReviewData($event->getUserId(), 'review_2', $event->getText());
+        updateUser($event->getUserId(), 'shop_review_3');
+    // 自由欄を登録
+    } else if ((getBeforeMessageByUserId($event->getUserId()) === 'shop_review_3')) {
+        updateReviewData($event->getUserId(), 'review_3', $event->getText());
+        updateUser($event->getUserId(), 'shop_review_confirm');
+    }
+
+    // usersテーブルのbefore_sendに設定されているメッセージに対する処理
+    if ((getBeforeMessageByUserId($event->getUserId()) != PDO::PARAM_NULL) && (getBeforeMessageByUserId($event->getUserId()) != null)) {
+        //shop_review
+        if (getBeforeMessageByUserId($event->getUserId()) === 'shop_review') {
+            //shopsテーブルにIDが存在するか確認
+            if (getShopNameByShopId($event->getText()) != PDO::PARAM_NULL) {
+                $shop = getShopNameByShopId($event->getText());
+                replyConfirmTemplate($bot, $event->getReplyToken(),
+                'レビュー確認',
+                $shop['shopname'].': この店のレビューを書きますか？',
+                new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
+                    'はい', 'はい'),
+                new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
+                    'キャンセル', 'キャンセル')
+                );
+                //entry review data
+                registerReviewDataFirst($event->getUserId(), $shop['shopid']);
+                updateUser($event->getUserId(), 'shop_review_0');
+            } else {
+                replyTextMessage($bot, $event->getReplyToken(),
+                '店が見つかりませんでした。正しいIDを入力して下さい。');
+            }
+        //shop_review_1
+        } else if (getBeforeMessageByUserId($event->getUserId()) === 'shop_review_1') {
+            // ボタンは4件までしかできないので入力してもらう
+            replyTextMessage($bot, $event->getReplyToken(), '総合の評価を1~5の5段階で入力してください。');
+        //shop_review_2
+        } else if (getBeforeMessageByUserId($event->getUserId()) === 'shop_review_2') {
+            replyTextMessage($bot, $event->getReplyToken(),
+            '食べたメニューまたはおすすめのメニューを入力して下さい。');
+        //shop_review_3
+        } else if (getBeforeMessageByUserId($event->getUserId()) === 'shop_review_3') {
+            replyTextMessage($bot, $event->getReplyToken(),
+            '備考等があれば入力して下さい。ない場合は「なし」と入力してください。');
+        //shop_review_confirm(レビュー内容の確認)
+        } else if (getBeforeMessageByUserId($event->getUserId()) === 'shop_review_confirm') {
+            if (strcmp($event->getText(), 'はい') == 0) {
+                //reviewsテーブルにreviewstockテーブルのデータを入れ、reviewstockのデータを削除
+                $row = getReviewStockData($event->getUserId());
+                registerReview($row['shopid'], $row['userid'], $row['review_1'], $row['review_2'], $row['review_3']);
+                deleteUser($event->getUserId(), TABLE_NAME_REVIEWSTOCK);
+                updateUser($event->getUserId(), null);
+                replyTextMessage($bot, $event->getReplyToken(),
+                'レビューを登録しました。');
+            } else {
+                replyConfirmTemplate($bot, $event->getReplyToken(),
+                'レビュー最終確認',
+                'レビューを登録しますか？',
+                new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
+                    'はい', 'はい'),
+                new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
+                    'キャンセル', 'キャンセル')
+                );
+            }
+        }
+
+        else if (getBeforeMessageByUserId($event->getUserId()) === 'shop_search') {
+            //件数を超えて次のページにいけないようにする
+            if (strcmp($event->getText(), '次へ') == 0) {
+                $page = getDataByUserShopData($event->getUserId(), 'page_num');
+                $range = getDataByUserShopData($event->getUserId(), 'shop_length');
+                //検索件数/PAGE_COUNT(切り上げ)よりも高い数字にならないようにする
+                if ($page <= ceil(floatval($range)/floatval(PAGE_COUNT))) {
+                    updateUserShopData($event->getUserId(), 'page_num', ($page+1));
+                    searchShop($event->getUserId(), $bot, $event->getReplyToken(), ($page+1));
+                } else {
+                    replyTextMessage($bot, $event->getReplyToken(), 'これ以上次へは進めません。');
+                }
+            }
+            //0ページよりも前にいけないようにする
+            else if (strcmp($event->getText(), '前へ') == 0) {
+                $page = getDataByUserShopData($event->getUserId(), 'page_num');
+                if ($page >= 1) {
+                    updateUserShopData($event->getUserId(), 'page_num', ($page-1));
+                    searchShop($event->getUserId(), $bot, $event->getReplyToken(), ($page-1));
+                } else {
+                    replyTextMessage($bot, $event->getReplyToken(), 'これ以上前には戻れません。');
+                }
+            }
+        }
+        
+    } 
+
+    //前のメッセージが登録されていない場合
+    else {
+        //searchshop
+        if(strcmp($event->getText(), 'お店を探す') == 0) {
+            // 登録された位置情報周辺のお店を探す
+            // 位置情報が設定されているかチェック
+            if(getLocationByUserId($event->getUserId()) != PDO::PARAM_NULL) {
+                searchShop($event->getUserId(), $bot, $event->getReplyToken());
+            } else {
+                replyButtonsTemplate($bot, $event->getReplyToken(), '位置情報の設定へ', 'https://'.$_SERVER['HTTP_HOST'].'/imgs/nuko.png', '位置情報の設定へ',
+                '位置情報が設定されていません。位置情報の設定をお願いします。',
+                new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('位置情報の設定へ', '位置情報の設定'),
+                );
+            }
+
+        //reviewshop
+        } else if(strcmp($event->getText(), 'レビュー登録') == 0) {
+            createUser($event->getUserId(), 'shop_review');
+            $id = getUserIdCheck($event->getUserId(), TABLE_NAME_USERS);
+            replyTextMessage($bot, $event->getReplyToken(),
+            // ユーザ登録、レビューはwebでさせる
+            'お店の番号を入力してください');
+
+        //locationset
+        } else if(strcmp($event->getText(), '位置情報の設定') == 0) {
+            replyButtonsTemplate($bot, $event->getReplyToken(), '位置情報の設定', 'https://'.$_SERVER['HTTP_HOST'].'/imgs/nuko.png', '位置情報の設定',
+            '位置情報の設定をします。下のボタンより位置情報を送って下さい。',
+            new LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder(
+                '位置情報の設定・変更', 'line://nv/location'),
+            new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
+                'キャンセル', 'キャンセル'),
+            );
+            createUser($event->getUserId(), 'location_set');
+
+        //setting
+        //あいさつメッセージでユーザ設定をさせる
+        } else if(strcmp($event->getText(), 'ユーザ設定') == 0) {
+            replyButtonsTemplate($bot, $event->getReplyToken(), 'ユーザ設定', 'https://'.$_SERVER['HTTP_HOST'].'/imgs/nuko.png', 'ユーザ設定',
+            'ユーザ設定をします。まずは位置情報の設定をお願いします。',
+            new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
+                '位置情報の設定・変更', 'line://nv/location'),
+            new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
+                'キャンセル', 'キャンセル'),
+            );
+        }
+
+    }
+}
+
+function createUser($userId, $beforeSend) {
+    //if not exists userid, entry userid
+    if(getUserIdCheck($userId, TABLE_NAME_USERS) === PDO::PARAM_NULL) {
+        registerUser($userId, $beforeSend);
+    } else {
+        //if already exists, update
+        updateUser($userId, $beforeSend);
+    }
+}
+
+function searchShop($userId, $bot, $token, $page=0) {
+    $location = getLocationByUserId($userId);
+    //カルーセルは5件まで
+    //1ページに5店表示(現在のページはデータベースに登録？)
+    $shopInfo = get_restaurant_information($location['latitude'], $location['longitude'], $page);
+    $columnArray = array();
+    for($i = 0; $i < count($shopInfo); $i++) {
+        //for文内でnavigationテーブルへのデータ追加をする
+        // registerNavigation(
+        //     $event->getUserId(),
+        //     $shopInfo[$i]["id"],
+        //     $shopInfo[$i]["number"],
+        //     $shopInfo[$i]["name"],
+        //     $shopInfo[$i]["latitude"],
+        //     $shopInfo[$i]["longitude"]
+        // );
+        $actionArray = array();
+        array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder (
+            '店舗情報', $shopInfo[$i]["url"]));
+        array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
+            'レビューを見る', 'https://'.$_SERVER['HTTP_HOST'].'/web/hello.html'));
+        array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
+            'レビューを書く', 'review_write_'.$shopInfo[$i]["number"].'_'.$shopInfo[$i]["id"]));
+        $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
+            $shopInfo[$i]["name"],
+            $shopInfo[$i]["number"].'/'.$shopInfo[$i]["shoplength"].'件:'.$shopInfo[$i]["genre"],
+            $shopInfo[$i]["image"],
+            $actionArray
+        );
+        array_push($columnArray, $column);
+    }
+    replyCarouselTemplate($bot, $token, 'お店を探す:'.($page+1).'ページ目', $columnArray);
+    updateUser($userId, 'shop_search');
+    if (getDataByUserShopData($userId, 'userid') != PDO::PARAM_NULL) {
+        updateUserShopData($userId, 'shop_length', $shopInfo[0]["shoplength"]);
+    } else {
+        registerUserShopData($userId, $shopInfo[0]["shoplength"]);
+    }
+}
+
+//CLASS//-----------------------------------------------------------
+// database_connection manage class
+class dbConnection {
+    // instance
+    protected static $db;
+    // constructor
+    private function __construct() {
+
+        try {
+            // get connection_infomation from environmentvaliable to database
+            $url = parse_url(getenv('DATABASE_URL'));
+            // data_source
+            $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+            // establish connection
+            self::$db = new PDO($dsn, $url['user'], $url['pass']);
+            // thrown Exception on error
+            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        }
+        catch (PDOException $e) {
+            error_log('Connection Error: ' . $e->getMessage());
+        }
+    }
+
+    // singleton. if not exists instance, create new one.
+    public static function getConnection() {
+        if (!self::$db) {
+            new dbConnection();
+        }
+    return self::$db;
+    }
+}
+?>
---
+++ app/search.php
@@ -0,0 +1,61 @@
+<?php
+function get_restaurant_information($lat, $lon, $page) {
+    $latitude = round($lat, 6);
+    $longitude = round($lon, 6);
+    //範囲も変えられるようにする？
+    $range = 2;
+    $start = $page * PAGE_COUNT;
+
+    // クエリをまとめる
+    $query = [
+        'key' => '7264b03648f65bd1',
+        'lat' => $latitude, // 緯度
+        'lng' => $longitude, // 経度
+        'range' => $range, // 検索範囲
+        'lunch' => 1,
+        'start' => ($start+1),
+        'count' => PAGE_COUNT,
+        'format' => 'json',
+    ];
+    // グルメサーチAPIからjsonを取得
+    $message = "";
+    $url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?';
+    $url .= http_build_query($query);
+    $response = file_get_contents($url);
+
+    $json = json_decode($response);
+    $data_array = renderJson($json, $start);
+
+    return $data_array;
+}
+
+function renderJson($json, $start) {
+    $resultLength = $json->{"results"}->{"results_available"};
+    if ($resultLength < 1) {
+        $result = "周辺にお店が見つかりませんでした。";
+        return $result;
+    }
+    $temp = $json->{"results"};
+    $resultTxt = "周辺500m以内に".$resultLength."件見つかりました。\r\n".PAGE_COUNT."件ごとに表示します。\r\n\n" . $result;
+
+    $data_array = array();
+    for ($i = 0; $i < PAGE_COUNT; $i++) {
+        if ($resultLength-$start <= $i) {
+            break;
+        }
+        $array = array($i => array(
+            "name" => $temp->{'shop'}[$i]->{'name'},
+            "id" => $temp->{'shop'}[$i]->{'id'},
+            "genre" => $temp->{'shop'}[$i]->{'genre'}->{'name'},
+            "url" => $temp->{'shop'}[$i]->{'urls'}->{'pc'},
+            "image" => $temp->{'shop'}[$i]->{'photo'}->{'mobile'}->{'s'},
+            "number" => $start+($i+1),
+            "latitude" => $temp->{'shop'}[$i]->{'lat'},
+            "longitude" => $temp->{'shop'}[$i]->{'lng'},
+            "shoplength" => $resultLength,
+        )); 
+        $data_array += $array;
+    }
+    return $data_array;
+}
+?>
