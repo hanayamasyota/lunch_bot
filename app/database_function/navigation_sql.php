2504fdbf77df6c85206342fa7b4b5780c6769862
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-09-08 17:20:52 +0900

    update-hanayama

--- app/database_function/navigation_sql.php
+++ app/database_function/navigation_sql.php
@@ -1,13 +1,13 @@
 <?php
 
 //navigationテーブルへのデータ挿入
-function registerNavigation($userId, $shopId, $shopNum, $shopName, $lat, $lng, $genre) {
+function registerNavigation($userId, $shopId, $shopNum, $shopName, $lat, $lng, $genre, $image, $url) {
     //到着時間を設定する
     $dbh = dbConnection::getConnection();
-    $sql = 'insert into '.TABLE_NAME_NAVIGATION.' (userid, shopid, shopnum, shopname, shop_lat, shop_lng, genre) 
-            values (pgp_sym_encrypt(?, \'' . getenv('DB_ENCRYPT_PASS') . '\'), ?, ?, ?, ?, ?, ?)';
+    $sql = 'insert into '.TABLE_NAME_NAVIGATION.' (userid, shopid, shopnum, shopname, shop_lat, shop_lng, genre, image, url) 
+            values (pgp_sym_encrypt(?, \'' . getenv('DB_ENCRYPT_PASS') . '\'), ?, ?, ?, ?, ?, ?, ?, ?)';
     $sth = $dbh->prepare($sql);
-    $sth->execute(array($userId, $shopId, $shopNum, $shopName, $lat, $lng, $genre));
+    $sth->execute(array($userId, $shopId, $shopNum, $shopName, $lat, $lng, $genre, $image, $url));
 }
 function checkShopByNavigation($userId, $shopNum) {
     $dbh = dbConnection::getConnection();
@@ -23,7 +23,7 @@ function checkShopByNavigation($userId, $shopNum) {
 }
 function getShopDataByNavigation($userId, $shopNum) {
     $dbh = dbConnection::getConnection();
-    $sql = 'select shopid, shopname, shopnum, shoplat, shoplng, image from ' . TABLE_NAME_NAVIGATION . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\') AND shopnum BETWEEN ? AND ?';
+    $sql = 'select shopid, shopname, shopnum, shop_lat, shop_lng, image, url from ' . TABLE_NAME_NAVIGATION . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\') AND shopnum BETWEEN ? AND ?';
     $sth = $dbh->prepare($sql);
     $sth->execute(array($userId, $shopNum, ($shopNum+4)));
     // if no record
