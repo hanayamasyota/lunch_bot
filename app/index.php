2e26c6a322de7518cff4bfb58a163a4c2ee534cf
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-09-09 16:15:33 +0900

    update-hanayama

--- app/index.php
+++ app/index.php
@@ -474,7 +474,7 @@ function showShop($page, $userId, $bot, $token) {
         $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
             $shop['shopname'],
             //何分かかるかを表示
-            $shop['shopnum'].'/'.$shopLength.'件:'.$shop['genge'],
+            $shop['shopnum'].'/'.$shopLength.'件:'.$shop['genre'],
             $shop['image'],
             $actionArray
         );
