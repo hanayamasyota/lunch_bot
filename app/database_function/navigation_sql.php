3f1afa367e4229f3e1b0314194a49c108611266b
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-09-16 15:13:03 +0900

    update-hanayama

--- app/database_function/navigation_sql.php
+++ app/database_function/navigation_sql.php
@@ -23,7 +23,7 @@ function checkShopByNavigation($userId, $shopNum) {
 }
 function getShopDataByNavigation($userId, $shopNum) {
     $dbh = dbConnection::getConnection();
-    $sql = 'select shopid, shopname, shopnum, shop_lat, shop_lng, genre, image, url from ' . TABLE_NAME_NAVIGATION . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\') AND shopnum BETWEEN ? AND ?';
+    $sql = 'select * from ' . TABLE_NAME_NAVIGATION . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\') AND shopnum BETWEEN ? AND ?';
     $sth = $dbh->prepare($sql);
     $sth->execute(array($userId, $shopNum, ($shopNum+4)));
     // if no record
