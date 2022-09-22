71084e9e245f9fd5abd5d492411a1795ae2b79fb
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-09-22 15:44:54 +0900

    update-hanayama

--- app/database_function/uservisitedshops_sql.php
+++ app/database_function/uservisitedshops_sql.php
@@ -9,7 +9,7 @@ function registerUserVisitedShops($userId, $shopId, $shopName, $time, $shopNum) 
 
 function getUserVisitedShopData($userId) {
     $dbh = dbConnection::getConnection();
-    $sql = 'select shopname, shopnum from'. TABLE_NAME_USERVISITEDSHOPS . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\') and ? = shopid';
+    $sql = 'select shopname, visittime, shopnum from'. TABLE_NAME_USERVISITEDSHOPS . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\')';
     $sth = $dbh->prepare($sql);
     $sth->execute(array($userId, $shopId));
     // if no record
@@ -31,6 +31,19 @@ function updateUserVisitedShops($userId, $shopId, $time) {
 function checkUserVisitedShops($userId, $shopId) {
     $dbh = dbConnection::getConnection();
     $sql = 'select * from '. TABLE_NAME_USERVISITEDSHOPS . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\') and ? = shopid';
+    $sth = $dbh->prepare($sql);
+    $sth->execute(array($userId, $shopId));
+    // if no record
+    if (!($row = $sth->fetch())) {
+        return PDO::PARAM_NULL;
+    } else {
+        return $row;
+    }
+}
+
+function getDataByUserVisitedShops($userId) {
+    $dbh = dbConnection::getConnection();
+    $sql = 'select shopname visittime shopnum from '. TABLE_NAME_USERVISITEDSHOPS . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\')';
     $sth = $dbh->prepare($sql);
     $sth->execute(array($userId, $shopId));
     // if no record
