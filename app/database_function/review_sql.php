cac9222a47ca2b5878fe086abb360cc7896e8e1c
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-09-28 12:32:53 +0900

    update-hanayama

--- app/database_function/review_sql.php
+++ app/database_function/review_sql.php
@@ -1,4 +1,9 @@
 <?php
+require_once '../DBConnection.php';
+require_once '../index.php';
+?>
+
+<?php
 function registerReview($userId, $shopId, $reviewNum, $review) {
     $dbh = dbConnection::getConnection();
     $sql = 'insert into '. TABLE_NAME_REVIEWS . ' (userid, shopid, review_num, review) values (pgp_sym_encrypt(?, \'' . getenv('DB_ENCRYPT_PASS') . '\'), ?, ?, ?) ';
