6dcb6a6b0834c2ec867854da65e6c53053070ab2
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-09-28 12:12:31 +0900

    update-hanayama

--- app/search.php
+++ app/search.php
@@ -119,7 +119,7 @@ function showShop($page, $userId, $bot, $token) {
         //     'レビューを見る', 'review_list_1'));
         array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder (
             //みんなのレビューを表示するページへ移動
-            'レビューを見る', SERVER_ROOT.'/web/base.php'));
+            'レビューを見る', SERVER_ROOT.'/web/review_list.php?shopid='.$shop['shopid']));
         array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
             //店までのナビゲーションを出したい
             'ここに行く!', 'visited_'.$shop['shopid'].'_'.$shop['shopname'].'_'.$shop['shopnum']));
