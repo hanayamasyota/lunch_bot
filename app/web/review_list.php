ad83d1d0b88db1a193c0341926941fc6eca99b9a
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-09-28 12:23:28 +0900

    update-hanayama

--- app/web/review_list.php
+++ app/web/review_list.php
@@ -47,7 +47,9 @@ foreach ( glob( $pattern ) as $filename )
 
         <?php
             $shopId = $_GET["shopid"];
+            echo $shopId;
             $reviewData = getReviewData($shopId);
+            echo $reviewData;
             $reviewArray_1 = array();
             $reviewArray_2 = array();
             $reviewArray_3 = array();
