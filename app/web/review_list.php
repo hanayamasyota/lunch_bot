f63f453984cdea8e5757474652485cb24c1a863e
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-09-30 17:14:36 +0900

    update-hanayama

--- app/web/review_list.php
+++ app/web/review_list.php
@@ -71,7 +71,7 @@ require_once '../database_function/review_sql.php';
     <!-- CONTENTS -->
     <div class="container dx-3 mt-5">
         <div class="bg-navy text-light">
-            <div class="px-2 mt-3 col-10 border-bottom-3">
+            <div class="px-2 pt-3 col-10 border-bottom-3">
                 <h3 class="h3"><?php echo $_GET["shopname"] ?></h3>
             </div>
             <div class="px-2">
@@ -87,7 +87,7 @@ require_once '../database_function/review_sql.php';
                 for ($i = 0; $i < count($reviewArray_1); $i++) { ?>
                 <table class="table table-bordered px-3">
                     <?php $time = explode(' ', $timeArray[$i])[0] ?>
-                    <thead class="bg-navy"><?php echo $time ?></thead>
+                    <thead><?php echo $time ?></thead>
                     <tr>
                         <th class="col-5 py-3 bg-lightorange text-dark">
                             レビュー項目１
