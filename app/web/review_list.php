3562c9d3d0f335ae6d01977ef7cc6672ea8ceb21
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-09-30 13:14:33 +0900

    update-hanayama

--- app/web/review_list.php
+++ app/web/review_list.php
@@ -57,13 +57,13 @@ require_once '../database_function/review_sql.php';
 <body id="page-top">
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
-        <div class="container px-5 pb-3">
+        <div class="container px-5">
             <p class="navbar-brand fw-bold">ひるまちぶらり</p>
         </div>
     </nav>
     <!-- Mashead header-->
     <header class="mt-5">
-        <div class="container px-3 pt-5 bg-color=#cccccc">
+        <div class="container px-3 pt-5 bg-light">
             <h2>みんなのレビュー</h2>
         </div>
     </header>
