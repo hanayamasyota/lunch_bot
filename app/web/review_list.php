6e8d9d9cab32e9acce33396377a8f607a544b841
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-03 17:14:37 +0900

    update-hanayama

--- app/web/review_list.php
+++ app/web/review_list.php
@@ -187,9 +187,9 @@ require_once '../database_function/review_sql.php';
         </div>
     </div>
     <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> -->
     <!-- Core theme JS-->
-    <script src="js/scripts.js"></script>
+    <!-- <script src="js/scripts.js"></script> -->
     <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
     <!-- * *                               SB Forms JS                               * *-->
     <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
