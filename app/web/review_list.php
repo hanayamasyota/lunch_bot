ec650e673da282788f5d1c50ba70941d6f427dc4
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-04 16:52:46 +0900

    update-hanayama

--- app/web/review_list.php
+++ app/web/review_list.php
@@ -58,7 +58,7 @@ require_once '../database_function/review_sql.php';
     <!-- Navigation-->
     <nav class="fixed-top shadow-sm" id="mainNav">
         <div class="container px-5">
-            <div class="h3 pt-3 font-syowa">ひるまち</div><div class="h3 pt-3 font-agent">GO</div>
+            <div class="h3 pt-3 font-syowa" white-space="nowrap">ひるまち</div><div class="h3 pt-3 font-agent">GO</div>
         </div>
     </nav>
     <!-- Mashead header-->
