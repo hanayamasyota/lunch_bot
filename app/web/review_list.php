9960ca5bbabdcb65a5eb3a6762753c1ced8c8d00
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-05 11:28:06 +0900

    update-hanayama

--- app/web/review_list.php
+++ app/web/review_list.php
@@ -69,7 +69,7 @@ require_once '../database_function/review_sql.php';
     </header>
 
     <!-- CONTENTS -->
-    <div class="container dx-2 mt-3 mb-5 bg-lightnavy">
+    <div class="container dx-2 my-5 bg-lightnavy">
         <div class="bg-navy text-light">
             <div class="px-2 pt-3 col-12 border-bottom-3">
                 <h3 class="h3"><?php echo $_GET["shopname"] ?></h3>
