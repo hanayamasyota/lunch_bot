3cd08dd3c933948df1a4174e29265dae987fc1cc
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-17 15:50:48 +0900

    update-hanayama

--- app/web/own_review_list.php
+++ app/web/own_review_list.php
@@ -93,6 +93,9 @@ if ($ownReviewData != PDO::PARAM_NULL) {
     <div class="container dx-2 my-5 bg-lightnavy">
     <div class="bg-white">
             <?php for ($i = 0; $i < count($scoreArray); $i++) { ?>
+                <div class="bg-navy text-light mb-0">
+                    <?php echo $shopNameArray[$i]; ?>
+                </div>
                 <table class="table border-navy px-3 bg-navy">
                     <?php $time = explode(' ', $timeArray[$i])[0] ?>
                     <thead><?php echo "レビュー日：".$time ?></thead>
