f8d52d3b7158d9028ff4c4b7635d5ce0788058bc
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-24 13:20:17 +0900

    update-hanayama

--- app/web/own_review_list.php
+++ app/web/own_review_list.php
@@ -22,6 +22,7 @@ if($_SERVER["REQUEST_METHOD"] != "POST") {
         $page = $_POST["page"];
 }
 $ownReviewData = getPageReviewData($userId, $page);
+error_log('count:'.count($ownReviewData));
 
 $reviewCount = 0;
 
