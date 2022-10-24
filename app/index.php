5f066b7f3d5ee30de6253fc10b581279cea1e550
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-24 11:39:01 +0900

    update-hanayama

--- app/index.php
+++ app/index.php
@@ -254,7 +254,7 @@ foreach ($events as $event) {
                 'userid' => $event->getUserId(),
                 'shopid' => $shop["shopid"],
                 'shopname' => $shop["shopname"],
-                'page' => 1;
+                'page' => 1,
                 );
                 $query = http_build_query($data);
 
