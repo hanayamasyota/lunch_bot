27091435cef732b338cc6dbbdf31753260e09577
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-26 16:07:50 +0900

    update-hanayama

--- app/database_function/owner_sql.php
+++ app/database_function/owner_sql.php
@@ -2,14 +2,14 @@
 
 function checkExistsEmail($email, $psword, $name) {
     $dbh = dbConnection::getConnection();
-    $sql = 'select email from ' .TABLE_NAME_. ' where ? = email';
+    $sql = 'select email from ' .TABLE_NAME_OWNER. ' where ? = email';
     $sth = $dbh->prepare($sql);
     $sth->execute(array($email));
 
     $row = $sth->fetch();
     
     if (!isset($row['email'])) {
-        $sql = 'insert into userDeta(owner_name, email, psword) value(?, ?, ?)';
+        $sql = 'insert into ' .TABLE_NAME_OWNER. ' (owner_name, email, psword) value (?, ?, ?)';
         $sth = $dbh->prepare($sql);
         $sth->execute(array($name, $email, $psword));
 
