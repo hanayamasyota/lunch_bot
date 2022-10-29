bca9910f92ea1232c802e9c2d4a44fc52dc0d600
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-29 20:43:36 +0900

    update-hanayama

--- app/database_function/review_sql.php
+++ app/database_function/review_sql.php
@@ -87,9 +87,9 @@ function getPageReviewData2($userId, $page) {
     $str = '';
     foreach($rows as $row) {
         if ($row === end($rows)) {
-            $str .= $row['shopid'].' = shopid';
+            $str .= $row['shopid']." = :'shopid'";
         } else {
-            $str .= $row['shopid'].' = shopid or ';
+            $str .= $row['shopid']." = :'shopid' or ";
         }
     }
     $sql = 'select * from ' .TABLE_NAME_REVIEWS. ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\') and (?) order by time';
