55d187f2f4993fc291655ce39cb2a4cb26bf3286
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-30 17:06:06 +0900

    update-hanayama

--- app/database_function/review_sql.php
+++ app/database_function/review_sql.php
@@ -76,7 +76,7 @@ function getReviewData($shopId, $conveni) {
 function getPageReviewData($userId, $page) {
     $start = ($page * ONE_PAGE - ONE_PAGE);
     $dbh = dbConnection::getConnection();
-    $sql = 'select distinct shopid from ' .TABLE_NAME_REVIEWS. ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\') order by shopid, time limit 5 offset ?';
+    $sql = 'select distinct shopid from ' .TABLE_NAME_REVIEWS. ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\') order by shopid limit 5 offset ?';
     $sth = $dbh->prepare($sql);
     $sth->execute(array($userId, $start));
     // if no record
