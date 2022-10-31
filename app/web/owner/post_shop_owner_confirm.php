67097f5b78ae5378f72a67111c93b67642234612
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-31 15:05:12 +0900

    update-hanayama

--- app/web/owner/post_shop_owner_confirm.php
+++ app/web/owner/post_shop_owner_confirm.php
@@ -23,9 +23,10 @@ define('TABLE_NAME_EVENTSHOPS', 'eventshops');
     $img_name = uniqid().'.png';
     error_log('imageName:'.$img_name);
     //画像を保存
+    error_log('temp_name='.$_FILES['photo']['name']);
+    error_log('name='.$_FILES['photo']['tmp_name']);
     if (move_uploaded_file($_FILES['photo']['tmp_name'], './photos/'.$img_name)) {
-        error_log('temp='.$_FILES['photo']['name']);
-        error_log('name='.$_FILES['photo']['tmp_name']);
+
         error_log('アップロードされたファイルを保存しました。');
     } else {
         error_log('アップロードされたファイルの保存に失敗しました。');
