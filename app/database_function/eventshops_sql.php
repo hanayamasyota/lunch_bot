073cc9934afb126983298f6547fd77bbe1f2bd4a
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-10-31 16:37:20 +0900

    update-hanayama

--- app/database_function/eventshops_sql.php
+++ app/database_function/eventshops_sql.php
@@ -1,10 +1,10 @@
 <?php
 
-function registerEventShopsByOwner($email, $eventMobilestore, $owner, $shopName, $img, $link, $holdStart, $holdEnd, $openTime, $closeTime, $genre, $feature, $lat, $lng) {
+function registerEventShopsByOwner($email, $owner, $eventMobilestore, $shopName, $img, $link, $holdStart, $holdEnd, $openTime, $closeTime, $genre, $feature, $lat, $lng) {
     $dbh = dbConnection::getConnection();
-    $sql = 'insert into ' . TABLE_NAME_EVENTSHOPS . ' (userid, event_mobilestore, owner, event_name, photo, url, open_date, close_date, open_time, close_time, genre, feature, latitude, longitude) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    $sql = 'insert into ' . TABLE_NAME_EVENTSHOPS . ' (userid, owner, event_mobilestore, event_name, photo, url, open_date, close_date, open_time, close_time, genre, feature, latitude, longitude) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
     $sth = $dbh->prepare($sql);
-    $sth->execute(array($email, $eventMobilestore, $owner, $shopName, $img, $link, $holdStart, $holdEnd, $openTime, $closeTime, $genre, $feature, $lat, $lng));
+    $sth->execute(array($email, $owner, $eventMobilestore, $shopName, $img, $link, $holdStart, $holdEnd, $openTime, $closeTime, $genre, $feature, $lat, $lng));
 }
 
 function getShopsEventsData($type) {
--- app/web/shop_list.php
+++ app/web/shop_list.php
@@ -47,7 +47,6 @@ error_log('count:'.count($shops));
     </header>
 
 
-
     <!-- Contents-->
     <div class="container mt-3 text-center py-5">
         <form method="post" action="#">
@@ -136,11 +135,6 @@ error_log('count:'.count($shops));
         </form>
     </div>
 
-
-
-
-
-
     <!-- Footer-->
     <footer class="bg-black text-center py-2 mt-5 fixed-bottom">
             <div class="container px-5">
@@ -148,83 +142,9 @@ error_log('count:'.count($shops));
                     <div class="mb-2">
                         ひるまちGO
                     </div>
-                    <!-- <a href="#!">Privacy</a>
-                    <span class="mx-1">&middot;</span>
-                    <a href="#!">Terms</a>
-                    <span class="mx-1">&middot;</span>
-                    <a href="#!">FAQ</a> -->
                 </div>
             </div>
         </footer>
-    
-    <!-- Feedback Modal-->
-    <div class="modal fade" id="feedbackModal" tabindex="-1" aria-labelledby="feedbackModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header bg-gradient-primary-to-secondary p-4">
-                    <h5 class="modal-title font-alt text-white" id="feedbackModalLabel">Send feedback</h5>
-                    <button class="btn-close btn-close-white" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body border-0 p-4"> -->
-                    <!-- * * * * * * * * * * * * * * *-->
-                    <!-- * * SB Forms Contact Form * *-->
-                    <!-- * * * * * * * * * * * * * * *-->
-                    <!-- This form is pre-integrated with SB Forms.-->
-                    <!-- To make this form functional, sign up at-->
-                    <!-- https://startbootstrap.com/solution/contact-forms-->
-                    <!-- to get an API token!-->
-                    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                        <!-- Name input-->
-                        <div class="form-floating mb-3">
-                            <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
-                            <label for="name">Full name</label>
-                            <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                        </div>
-                        <!-- Email address input-->
-                        <div class="form-floating mb-3">
-                            <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
-                            <label for="email">Email address</label>
-                            <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                            <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                        </div>
-                        <!-- Phone number input-->
-                        <div class="form-floating mb-3">
-                            <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
-                            <label for="phone">Phone number</label>
-                            <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                        </div>
-                        <!-- Message input-->
-                        <div class="form-floating mb-3">
-                            <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
-                            <label for="message">Message</label>
-                            <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                        </div>
-                        <!-- Submit success message-->
-                        <!---->
-                        <!-- This is what your users will see when the form-->
-                        <!-- has successfully submitted-->
-                        <div class="d-none" id="submitSuccessMessage">
-                            <div class="text-center mb-3">
-                                <div class="fw-bolder">Form submission successful!</div>
-                                To activate this form, sign up at
-                                <br />
-                                <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                            </div>
-                        </div>
-                        <!-- Submit error message-->
-                        <!---->
-                        <!-- This is what your users will see when there is-->
-                        <!-- an error submitting the form-->
-                        <div class="d-none" id="submitErrorMessage">
-                            <div class="text-center text-danger mb-3">Error sending message!</div>
-                        </div>
-                        <!-- Submit Button-->
-                        <div class="d-grid"><button class="btn btn-primary rounded-pill btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
@@ -235,58 +155,5 @@ error_log('count:'.count($shops));
     <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
     <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
-
-<!-- <script>
-        $("#input1").on("change", function (e) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $("#sample1").attr("src", e.target.result);
-            }
-            reader.readAsDataURL(e.target.files[0]);
-        });
-
-        function Switch() {
-            const radio = document.getElementsByName('radio1');
-            const shop = document.getElementsByClassName('shopList');
-            const event = document.getElementsByClassName('eventList');
-            if (radio[0].checked) {
-                shop[0].style.display = '';
-                shop[1].style.display = '';
-                event[0].style.display = 'none';
-                event[1].style.display = 'none';
-                
-            }
-            else if (radio[1].checked) {
-                shop[0].style.display = 'none';
-                shop[1].style.display = 'none';
-                event[0].style.display = '';
-                event[1].style.display = '';
-            }
-            else {
-                shop[0].style.display = 'none';
-                shop[1].style.display = 'none';
-                event[0].style.display = 'none';
-                event[1].style.display = 'none';
-            }
-        }
-
-        window.onload = Switch();
-
-
-        
-
-
-        // $('#myImage').on('change', function (e) {
-        //     var reader = new FileReader();
-        //     reader.onload = function (e) {
-        //         $("#preview").attr('src', e.target.result);
-        //     }
-        //     reader.readAsDataURL(e.target.files[0]);
-        // });
-
-</script> -->
-
-
-
 
 </html>
