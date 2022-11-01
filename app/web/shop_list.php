b6cd5ca8a1331006e570c19f6062bdd73264003e
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-11-01 12:46:43 +0900

    update-hanayama

--- app/web/shop_list.php
+++ app/web/shop_list.php
@@ -71,7 +71,7 @@ error_log('count:'.count($shops));
                     // $imginfo = getimagesize('data:application/octet-stream;base64,' . $shop["photo"]);
                     // error_log(print_r($imginfo, true));
                     ?>
-                    <img src="<?php echo 'data:image/png;base64,'.$shop[0]["photo"].';'; ?>"> 
+                    <img src="<?php echo 'data:image/png;base64,'.$shop["photo"][0].';'; ?>"> 
                 </td>
             </tr>
 
