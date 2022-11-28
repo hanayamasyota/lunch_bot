bb23509555fcf93059341a7a6204d8dc92649541
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-11-28 17:05:20 +0900

    hirumachigo

--- app/web/score.php
+++ app/web/score.php
@@ -10,6 +10,7 @@ define('TABLE_NAME_USERLEGENDS', 'user_legends');
 <?php
 $userId = '';
 $score = 0;
+$legends = array();
 if ($_SERVER["REQUEST_METHOD"] != "GET") {
     $userId = $_GET["userid"];
     //登録数を取得
