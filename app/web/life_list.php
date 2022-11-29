d1a1e8e299638022c9f43f22385ca374dccad888
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-11-29 16:28:41 +0900

    hirumachigo

--- app/web/life_list.php
+++ app/web/life_list.php
@@ -1,11 +1,15 @@
 <?php 
 require_once '../DBConnection.php';
+require_once '../database_function/users_sql.php';
+require_once '../database_function/legends_sql.php';
 require_once '../database_function/eventshops_sql.php';
 require_once '../database_function/genre_sql.php';
 require_once 'list.php';
 
 define('TABLE_NAME_EVENTSHOPS', 'eventshops');
 define('TABLE_NAME_GENRE', 'genre');
+define('TABLE_NAME_USERS', 'users');
+define('TABLE_NAME_USERLEGENDS', 'user_legends');
 ?>
 
 <?php
@@ -67,11 +71,32 @@ if ($shops != 0) {
     if ($shops != 0) {
         foreach($shops as $shop) {
     ?>
+        <div class="container px-3 py-3 mb-3 bg-navy text-light h2">
+            <?php echo $shop["event_name"]; ?>
+        </div>
         <table class="table border-top border-navy align-middle mb-5 text-nowrap">
-
-            <div class="container px-3 py-3 mb-3 bg-navy text-light h2">
-                <?php echo $shop["event_name"]; ?>
-            </div>
+            <thead>
+                <div>
+                    <?php
+                    $userId = $shop["userid"];
+                    $now_legend = getNowLegend($userId);
+                    $name = '';
+                    ?>
+                    <?php if ($shop["owner"] == true) { ?>
+                        <?php 
+                            $legend = getLegends($now_legend);
+                            echo '<div class="bg-navy text-light d-inline px-2">オーナー</div>'; 
+                        ?>
+                    <?php } else if (isset($now_legend)) { ?>
+                        <?php 
+                            $legend = getLegends($now_legend);
+                            $name = getNickNameByUserId($userId);
+                            echo '<div class="bg-navy text-light d-inline px-2">'.$legend.'</div>'; 
+                        ?>
+                    <?php } ?>
+                    <?php echo ' '.$name; ?><small>さん</small></div><?php echo "レビュー日：" . $shop["time"]; ?>
+                </div>
+            </thead>
 
             <?php if (isset($shop["photo"])) { ?>
             <tr>
