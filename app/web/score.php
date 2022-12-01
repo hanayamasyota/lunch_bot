1d3f95c9a4fff6ee0eba14651bef93117a95672d
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-12-01 14:36:57 +0900

    hirumachigo

--- app/web/score.php
+++ app/web/score.php
@@ -116,8 +116,8 @@ $legends = getUserLegends($userId);
 
     <h2 class="mt-2 mb-2 text-center">称号獲得履歴</h2>
     <hr>
-    <div class="scroll col-10 py-0 mb-5 mx-auto border border-2 bg-light">
-        <p class="text-start col-12 border-bottom"><small>こちらにログが表示されます</small></p>
+    <div class="scroll col-10 pb-2 mb-5 mx-auto border border-2 bg-light">
+        <p class="text-start col-12 border-bottom text-gray mb-2"><small>こちらにログが表示されます</small></p>
         <?php if (!($legends == PDO::PARAM_NULL)) { //取得ログを表示する ?>
             <?php foreach ($legends as $legend) { ?>
                 <?php $name = getLegends($legend['legend_id']); ?>
