e57f2322791b1281c059cc64b46878e4bf92a038
hanayamasyota <fki2166291@stu.o-hara.ac.jp>
2022-12-02 11:03:05 +0900

    hirumachigo

--- app/web/owner/owner_index.php
+++ app/web/owner/owner_index.php
@@ -44,15 +44,15 @@ if (!(isset($_SESSION['email']))) {
     </header>
 
     <!-- CONTENTS -->
-    <div class="container dx-3 my-5 bg-lightnavy col-12">
-        <ul class="mx-2 contents align-center col-12">
+    <div class="container dx-3 my-5 bg-lightnavy w-80">
+        <ul class="mx-2 contents align-center w-100">
             <label for="shop">
-                <div class="col-12">
+                <div class="w-100">
                     <li class="contents_list"><a href="post_shop_owner.php" id="shop">実店舗(固定店舗)の登録</a></li>
                 </div>
             </label>
             <label for="event">
-                <div class="col-12">
+                <div class="w-100">
                     <li class="contents_list"><a href="post_event_owner.php" id="event">イベント・移動店舗の登録</a></li>
                 </div>
             </label>
